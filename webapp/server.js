c5c012d317af2a970fffc0307576446a3d1623f3
gautam0707 <bgautam.0707@gmail.com>
2017-01-02 23:54:56 +0530

    separation of concerns

--- webapp/server.js
+++ webapp/server.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+app.use(express.static('./public/'));
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(8080);
