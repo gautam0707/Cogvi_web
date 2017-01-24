5195e59d3b2162ee038b80755b2626154d71cadd
gautam0707 <bgautam.0707@gmail.com>
2017-01-24 21:01:38 +0530

    added wiki page

---
+++ wiki/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var expressObj = express();
+var port = 8080;
+
+expressObj.use(express.static('public'));
+expressObj.get('/',function(req, resp){
+    resp.sendFile('./public/index.html')
+});
+expressObj.listen(port, function (err) {
+    if (err) {
+        console.log('something went wrong..' + err);
+    } else {
+        console.log('Listening on port ' + port);
+        }
+});
